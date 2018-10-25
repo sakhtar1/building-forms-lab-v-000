96f475ce138770c76f719059ef71c0ff3a90ef72
Sharmina Akhtar <sakht318@gmail.com>
2018-10-25 06:50:38 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -6,7 +6,7 @@ export default function manageBand(state = {
   switch (action.type) {
     case 'ADD_BAND':
 
-      return { bands: state.bands.concat(action.payload.name)}
+      return { bands: state.bands.concat(action.payload.name) };
 
     default:
       return state;
